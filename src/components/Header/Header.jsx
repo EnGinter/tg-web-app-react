951dea145ca276a25a591a0b786f3e6f5d14e3c5
Enjey Ginter <celedestinohmygod@gmail.com>
2022-11-29 23:20:00 +0530

    Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,9 @@ const Header = () => {
         <header className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>{user?.username}</span>
+            <div>
+                {user}
+            </div>
         </header>
     );
 };
